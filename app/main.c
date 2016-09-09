b9637334b5463ec67f429213833d7a0de51dcd3c
Liu Peng <pengliu@credosemi.com>
2016-09-09 16:09:14 +0800

    add tiny printf

--- app/main.c
+++ app/main.c
@@ -1,71 +1,49 @@
 #include "stm8s.h"
+#include "printf.h"
 
-/**
-  * @addtogroup TIM1_Input_Capture
-  * @{
-  */
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 uint32_t TIM1ClockFreq = 2000000;
-__IO uint32_t LSIClockFreq = 0;
-uint16_t ICValue1 =0, ICValue2 =0;
-/* Private function prototypes -----------------------------------------------*/
-static void TIM1_Config(void);
-static int putchar(char c);
-/* Private functions ---------------------------------------------------------*/
-/* Public functions ----------------------------------------------------------*/
+uint32_t LSIClockFreq = 0;
+uint16_t ICValue1 = 0;
+uint16_t ICValue2 =0;
 
-/**
-  * @brief  Main program.
-  * @param  None
-  * @retval None
-  */
+static void TIM1_Config(void);
+void putchar(char c);
+
 void main(void)
 {
-	//unsigned long int d;
-
-	/* for debug */
-	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
-	UART1_DeInit();
-	UART1_Init((uint32_t)115200, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO,
-	              UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
+	unsigned long int d;
 
   /* Connect LSI to COO pin*/ /* PC4: CCO */
   GPIO_Init(GPIOC, GPIO_PIN_4, GPIO_MODE_OUT_PP_LOW_FAST);
   CLK_CCOConfig(CLK_OUTPUT_LSI);
   CLK_CCOCmd(ENABLE);
 
-  /* TIM1 configuration -----------------------------------------*/
+  /* TIM1 configuration */
   TIM1_Config();
     
   /* Compute LSI clock frequency */
   LSIClockFreq = (8 * TIM1ClockFreq) / (ICValue2 - ICValue1);
   
+	/* for debug */
+	CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+	UART1_DeInit();
+	UART1_Init((uint32_t)115200, UART1_WORDLENGTH_8D, UART1_STOPBITS_1, UART1_PARITY_NO,
+	             UART1_SYNCMODE_CLOCK_DISABLE, UART1_MODE_TXRX_ENABLE);
+	printf("Masure Clock: %u %u %u\n", LSIClockFreq, ICValue1, ICValue2);
+
 	// Configure pins
-	//PE_DDR = 0x20;
-	//PE_CR1 = 0x20;
-	// Loop
-	//do {
-	//	PE_ODR ^= 0x20;
-	//	for(d = 0; d < 255000; d++) { }
-	//	//for(d = 0; d < LSIClockFreq; d++) { }
-	//} while(1);
-	putchar('H');
-	putchar('e');
-	putchar('L');
-	putchar('L');
-	putchar('o');
-	putchar('\n');
+	PE_DDR = 0x20;
+	PE_CR1 = 0x20;
+	do {
+		PE_ODR ^= 0x20;
+		for(d = 0; d < LSIClockFreq; d++) { }
+	} while(1);
 }
 
-static int putchar(char c)
+void putchar(char c)
 {
 	UART1_SendData8(c);
 	while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET);
-	return c;
 }
 
 /**
