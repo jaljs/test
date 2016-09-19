781e4503e8616a8f2f5a777171da52921a6607ad
Liu Peng <pengliu@credosemi.com>
2016-09-19 21:25:42 +0800

    add time_sync app for stm8l151x

---
+++ app/time_sync/main.c
@@ -0,0 +1,54 @@
+#include "stm8l15x.h"
+#include "printf.h"
+#include "stm8l15x_usart.h"
+#include "stm8l15x_gpio.h"
+#include "stm8l15x_clk.h"
+
+void putchar(char c);
+static void GPIO_Config(void);
+
+#define GPIO_LED_PORT (GPIOA)
+#define GPIO_LED_PIN  (GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7)
+
+void putchar(char c);
+void Delay(uint32_t nCount);
+
+void main(void)
+{
+	GPIO_Config();
+
+  CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
+	USART_DeInit(USART1);
+	USART_Init(
+    USART1,
+    (uint32_t)115200,
+    USART_WordLength_8b,
+    USART_StopBits_1,
+    USART_Parity_No,
+    USART_Mode_Rx | USART_Mode_Tx
+  );
+
+	while(1) {
+    GPIO_ToggleBits(GPIO_LED_PORT, GPIO_LED_PIN);
+    Delay(0x80000);
+	}
+}
+
+void putchar(char c)
+{
+	USART_SendData8(USART1, c);
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+}
+
+void Delay(uint32_t nCount)
+{
+  while (nCount != 0) {
+    nCount--;
+  }
+}
+
+static void GPIO_Config(void)
+{
+	/* LED */
+	GPIO_Init(GPIO_LED_PORT, GPIO_LED_PIN, GPIO_Mode_Out_PP_Low_Fast);
+}
