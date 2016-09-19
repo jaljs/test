b1a5102db098029bcd0afa5bca1513192823878e
Liu Peng <pengliu@credosemi.com>
2016-09-19 23:05:41 +0800

    split USART/CLK config to funcs.

--- app/time_sync/main.c
+++ app/time_sync/main.c
@@ -4,29 +4,21 @@
 #include "stm8l15x_gpio.h"
 #include "stm8l15x_clk.h"
 
-void putchar(char c);
-static void GPIO_Config(void);
-
 #define GPIO_LED_PORT (GPIOA)
 #define GPIO_LED_PIN  (GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7)
 
 void putchar(char c);
 void Delay(uint32_t nCount);
 
+void CLK_Config(void);
+void GPIO_Config(void);
+void USART_Config(void);
+
 void main(void)
 {
+  CLK_Config();
+  USART_Config();
 	GPIO_Config();
-
-  CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
-	USART_DeInit(USART1);
-	USART_Init(
-    USART1,
-    (uint32_t)115200,
-    USART_WordLength_8b,
-    USART_StopBits_1,
-    USART_Parity_No,
-    USART_Mode_Rx | USART_Mode_Tx
-  );
 
 	while(1) {
     GPIO_ToggleBits(GPIO_LED_PORT, GPIO_LED_PIN);
@@ -51,4 +43,22 @@ static void GPIO_Config(void)
 {
 	/* LED */
 	GPIO_Init(GPIO_LED_PORT, GPIO_LED_PIN, GPIO_Mode_Out_PP_Low_Fast);
+}
+
+static void CLK_Config(void)
+{
+  CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
+}
+
+static void USART_Config(void)
+{
+	USART_DeInit(USART1);
+	USART_Init(
+    USART1,
+    (uint32_t)115200,
+    USART_WordLength_8b,
+    USART_StopBits_1,
+    USART_Parity_No,
+    USART_Mode_Rx | USART_Mode_Tx
+  );
 }
