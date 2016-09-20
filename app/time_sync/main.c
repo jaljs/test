de17d392f8afa5b102354e1f237c3a771caf2750
Liu Peng <pengliu@credosemi.com>
2016-09-20 19:18:19 +0800

    Improve clk counter && add Discovery Win Finder
    - Improve sys_clk_precise
    
    use 16 prescaler. so 1 tick is 1us
    
    16MHz/16 = 1MHz
    PERIOD = 0xFFFF -> 65.536ms (trigger a TIM2 overflow in 65.526ms. sys_clk_high++)
    the sys_clk_low will directly read from TIM2_GetCounter()
    
    - Discovery Win Finder(100us)

--- app/time_sync/main.c
+++ app/time_sync/main.c
@@ -1,13 +1,15 @@
+#include "stdio.h"
+
 #include "stm8l15x.h"
-#include "printf.h"
 #include "stm8l15x_usart.h"
 #include "stm8l15x_gpio.h"
 #include "stm8l15x_clk.h"
 #include "stm8l15x_tim2.h"
+#include "stm8l15x_exti.h"
 
 #define GPIO_LED_PORT (GPIOA)
 #define GPIO_LED_PIN  (GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7)
-#define TIM2_PERIOD 1249
+#define TIM2_PERIOD 0xFFFF
 
 #define GPIO_TX_WIN_PORT GPIOB
 #define GPIO_TX_WIN_IN GPIO_Pin_7
@@ -15,18 +17,22 @@
 #define GPIO_TX_WIN_TIME_SYNC_OUT GPIO_Pin_5
 
 enum {
-  STATE_TRIG_TX_WIN_FL_EG_1 = 1,
-  STATE_TRIG_TX_WIN_FL_EG_2,
-  STATE_TRIG_TX_WIN_FL_EG_3,
+  State_Calc_Falling_Edge_1 = 1,
+  State_Calc_Falling_Edge_2,
+  State_Calc_Falling_Edge_3,
 };
 
-static uint32_t sys_clk_count;
-static uint8_t state_tx_win;
+static uint16_t sys_clk_high;
+
+static uint8_t state;
+static uint32_t last_clk_diff;
+static uint32_t last_clk_count;
 
 void putchar(char c);
 
 static void Delay(uint32_t nCount);
 static void enable_interrupts(void);
+static uint32_t clk_diff(uint32_t current, uint32_t last);
 
 static void CLK_Config(void);
 static void GPIO_Config(void);
@@ -45,10 +51,10 @@ void main(void)
   initialize();
 
   enable_interrupts();
-
-	while(1) {
+  while(1) {
     Delay(0x80000);
-	}
+    printf("clk:%04x%04x\n", sys_clk_high, TIM2_GetCounter());
+  }
 }
 
 void putchar(char c)
@@ -71,28 +77,48 @@ static void enable_interrupts(void)
 
 static void initialize(void)
 {
-  sys_clk_count = 0;
-  state_tx_win = STATE_TRIG_TX_WIN_FL_EG_1;
+  sys_clk_high = 0;
+  state = State_Calc_Falling_Edge_1;
+}
+
+static uint32_t clk_diff(uint32_t current, uint32_t last)
+{
+  if (current > last)
+    return current - last;
+  else
+    return TIM2_PERIOD - last + current;
 }
 
 static void GPIO_Config(void)
 {
 	/* LED */
 	GPIO_Init(GPIO_LED_PORT, GPIO_LED_PIN, GPIO_Mode_Out_PP_Low_Fast);
+
+  /* Discovery WIN Event */
+  GPIO_Init(GPIO_TX_WIN_PORT, GPIO_TX_WIN_DISCOVERY_WIN_EVENT, GPIO_Mode_Out_PP_Low_Fast);
+
+  /* Tx WIN Input (Interrupt) */
+  GPIO_Init(GPIO_TX_WIN_PORT, GPIO_TX_WIN_IN, GPIO_Mode_In_FL_IT);
+  EXTI_SetPinSensitivity(EXTI_Pin_7, EXTI_Trigger_Falling);
 }
 
 static void CLK_Config(void)
 {
   CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
+  CLK_SYSCLKSourceSwitchCmd(ENABLE);
+  CLK_SYSCLKSourceConfig(CLK_SYSCLKSource_HSE);
 
   /* Enable TIM2 clock */
   CLK_PeripheralClockConfig(CLK_Peripheral_TIM2, ENABLE);
+
+  /* Enable USART clock */
+  CLK_PeripheralClockConfig(CLK_Peripheral_USART1, ENABLE);
 }
 
 static void USART_Config(void)
 {
-	USART_DeInit(USART1);
-	USART_Init(
+  USART_DeInit(USART1);
+  USART_Init(
     USART1,
     (uint32_t)115200,
     USART_WordLength_8b,
@@ -104,10 +130,10 @@ static void USART_Config(void)
 
 static void TIM2_Config(void)
 {
-  /* 16M/128 = 125000HZ
-     TIM2_PERIOD = 0.01s * 125000 - 1 = 1249
+  /* 16M/16 = 1MHz
+     1/1MHz = 1us
    */
-  TIM2_TimeBaseInit(TIM2_Prescaler_128, TIM2_CounterMode_Up, TIM2_PERIOD);
+  TIM2_TimeBaseInit(TIM2_Prescaler_16, TIM2_CounterMode_Up, TIM2_PERIOD);
 
   /* Clear TIM2 update flag */
   TIM2_ClearFlag(TIM2_FLAG_Update);
@@ -123,17 +149,51 @@ void exti4_isr(void) __interrupt(12)
 {
 }
 
+/* EXTI7 GPIOB Pin_7 -> Tx Win Input */
 void exti7_isr(void) __interrupt(15)
 {
+  uint32_t curr_clk_diff;
+  uint32_t clk_count;
 
+  clk_count = TIM2_GetCounter();
+  switch(state) {
+  case State_Calc_Falling_Edge_1:
+    state = State_Calc_Falling_Edge_2;
+    break;
+  case State_Calc_Falling_Edge_2:
+    last_clk_diff = clk_diff(clk_count, last_clk_count);
+    state = State_Calc_Falling_Edge_3;
+    break;
+  case State_Calc_Falling_Edge_3:
+    /*
+       100 -> 100us
+    */
+    curr_clk_diff = clk_diff(clk_count, last_clk_count);
+    if (curr_clk_diff > last_clk_diff && ((curr_clk_diff - last_clk_diff) > 100)) {
+       GPIO_ToggleBits(GPIO_LED_PORT, GPIO_LED_PIN);
+
+       /* Trigger Discovery Event */
+       GPIO_ToggleBits(GPIO_TX_WIN_PORT, GPIO_TX_WIN_DISCOVERY_WIN_EVENT);
+           
+       state = State_Calc_Falling_Edge_1;
+       clk_count = 0;
+    } else if (curr_clk_diff < last_clk_diff && ((last_clk_diff - curr_clk_diff) > 100)) {
+      state = State_Calc_Falling_Edge_1;
+      clk_count = 0;
+    } else {
+      last_clk_diff = clk_diff(clk_count, last_clk_count);
+    }
+    break;
+  }
+
+  /* Always update last_clk_count */
+  last_clk_count = clk_count;
+
+  EXTI_ClearITPendingBit(EXTI_IT_Pin7);
 }
 
 void tim2_upd_ovf(void) __interrupt(19)
 {
-  sys_clk_count++; 
-  if (sys_clk_count == 100) {
-    GPIO_ToggleBits(GPIO_LED_PORT, GPIO_LED_PIN);
-    sys_clk_count = 0;
-  }
+  sys_clk_high++; 
   TIM2_ClearITPendingBit(TIM2_IT_Update);
 }
